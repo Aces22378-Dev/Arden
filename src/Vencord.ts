b8b01308e8e9bbadcaef8cf3c9aed7f8dae90eea
Aces22378-Dev <aces2378@gmail.com>
2026-09-23 16:00:16 -0400

    Rebrand desktop update messages to Arden

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -147,7 +147,7 @@ async function runUpdateCheck() {
                 notifiedForUpdatesThisSession = true;
 
                 showNotice(
-                    "Equicord has been updated!",
+                    "Arden has been updated!",
                     "Restart",
                     relaunch
                 );
@@ -192,7 +192,7 @@ function initTrayIpc() {
             await update();
             relaunch();
         } catch (err) {
-            UpdateLogger.error("Failed to repair Equicord", err);
+            UpdateLogger.error("Failed to repair Arden", err);
         }
     });
 
@@ -222,7 +222,7 @@ async function init() {
                 "Webpack has finished initialising, but some patches haven't been applied yet.",
                 "This might be expected since some Modules are lazy loaded, but please verify",
                 "that all plugins are working as intended.",
-                "You are seeing this warning because this is a Development build of Equicord.",
+                "You are seeing this warning because this is a Development build of Arden.",
                 "\nThe following patches have not been applied:",
                 "\n\n" + pendingPatches.map(p => `${p.plugin}: ${p.find}`).join("\n")
             );
