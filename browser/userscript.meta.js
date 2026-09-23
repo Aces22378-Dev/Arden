4167d96f7938ddcecd28d10fc8366256794ca95a
Aces22378-Dev <aces2378@gmail.com>
2026-09-23 15:58:14 -0400

    Rebrand web extension to Arden

--- browser/userscript.meta.js
+++ browser/userscript.meta.js
@@ -1,11 +1,10 @@
-// ==UserScript==
-// @name            Equicord
+// @name            Arden
 // @description     A Discord client mod - Web version
 // @version         %version%
-// @author          Equicord (https://github.com/Equicord)
-// @namespace       https://github.com/Equicord/Equicord
-// @supportURL      https://github.com/Equicord/Equicord
-// @icon            https://raw.githubusercontent.com/Equicord/Equicord/refs/heads/main/browser/icon.png
+// @author          Aces22378-Dev
+// @namespace       https://github.com/Aces22378-Dev/Arden
+// @supportURL      https://github.com/Aces22378-Dev/Arden
+// @icon            https://raw.githubusercontent.com/Aces22378-Dev/Arden/refs/heads/main/browser/icon.png// @icon            https://raw.githubusercontent.com/Equicord/Equicord/refs/heads/main/browser/icon.png
 // @license         GPL-3.0
 // @match           *://*.discord.com/*
 // @grant           GM_xmlhttpRequest
@@ -23,4 +22,4 @@
 // to overwrite stuff on the window on sites that use CSP. Use Tampermonkey or use a chromium based browser
 // https://github.com/violentmonkey/violentmonkey/issues/997
 
-// this is a compiled and minified version of Equicord. For the source code, visit the GitHub repo
+// this is a compiled and minified version of Arden. For the source code, visit the GitHub repo
